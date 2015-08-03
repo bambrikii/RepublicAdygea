fa729a4aec54dcda6968fcf9dcf003b3178c9070
Alexander Arakelyan <arakelyan@sovzond.ru>
2015-08-03 10:11:45 +0300

    - LandAreaTypes.

--- Справочники/LandAreaTypes.sql
+++ Справочники/LandAreaTypes.sql
@@ -1,5 +1,5 @@
-TRUNCATE TABLE lands_land_area_type;
-INSERT INTO lands_land_area_type (id, name) VALUES (nextval('lands_land_area_type'), 'Уточнённая');
-INSERT INTO lands_land_area_type (id, name) VALUES (nextval('lands_land_area_type'), 'Декларированная');
-INSERT INTO lands_land_area_type (id, name) VALUES (nextval('lands_land_area_type'), 'По карте');
-INSERT INTO lands_land_area_type (id, name) VALUES (nextval('lands_land_area_type'), 'Под застройкой');
+﻿TRUNCATE TABLE lands_land_area_type;
+INSERT INTO lands_land_area_type (id, name) VALUES (nextval('lands_land_area_type_seq'), 'Уточнённая');
+INSERT INTO lands_land_area_type (id, name) VALUES (nextval('lands_land_area_type_seq'), 'Декларированная');
+INSERT INTO lands_land_area_type (id, name) VALUES (nextval('lands_land_area_type_seq'), 'По карте');
+INSERT INTO lands_land_area_type (id, name) VALUES (nextval('lands_land_area_type_seq'), 'Под застройкой');
